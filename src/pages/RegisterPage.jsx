b04a8741075cac07f01fa5fe798cfc5e8899ee10
Paz Gueta <pazgueta@gmail.com>
2024-07-04 18:18:11 +0300

    added registeration

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 
 import {
   Card,
@@ -9,10 +10,56 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { LogIn } from "lucide-react";
+import { useState } from "react";
+import api from "@/services/api.service";
 
 function RegisterPage() {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(""); 
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+    try {
+      setLoading(true);
+      const newUser = {
+        username: formData.username,
+        password: formData.password,
+        confirmPassword: formData.confirmPassword,
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+      };
+      await api.post(`auth/register`, newUser);
+      navigate('/auth/login')
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setError("User already exists. Please choose a different username.");
+      } else {
+        setError("An error occurred during registration. Please try again.");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <Card className="shadow-2xl min-w-96">
       <CardHeader>
@@ -21,24 +68,47 @@ function RegisterPage() {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div>
             <Label>Username:</Label>
-            <Input placeholder="Enter username..." />
+            <Input 
+              placeholder="Enter username..." 
+              name="username"
+              value={formData.username}
+              onChange={handleChange}/>
           </div>
           <div>
             <Label>Password:</Label>
-            <Input type="password" placeholder="Enter password..." />
+            <Input type="password" 
+              placeholder="Enter password..." 
+              name="password"
+              value={formData.password}
+              onChange={handleChange}/>
+          </div>
+          <div>
+            <Label>Password:</Label>
+            <Input type="password" 
+              placeholder="Enter password again..." 
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}/>
           </div>
           <div>
             <Label>First name:</Label>
-            <Input placeholder="Enter first name..." />
+            <Input 
+              placeholder="Enter first name..." 
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}/>
           </div>
           <div>
             <Label>Last name:</Label>
-            <Input placeholder="Enter last name..." />
+            <Input 
+              placeholder="Enter last name..." 
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}/>
           </div>
-
           <Button>Register</Button>
         </form>
       </CardContent>
